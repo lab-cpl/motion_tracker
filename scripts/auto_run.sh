200788f720c42a72e39e5d1b5546304d7dddbfd0
nicolasluarte <l.luarte@uc.cl>
2025-03-08 21:31:49 -0300

    update

--- scripts/auto_run.sh
+++ scripts/auto_run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 sudo chmod +x /etc/rc.local
-sudo echo "sudo bash -c '/home/pi/git_pull.sh'" > /etc/rc.local
+sudo echo "sudo bash -c '/home/pi/git_pull.sh'
+echo "running rc.local" > /tmp/rc_test.txt" > /etc/rc.local
