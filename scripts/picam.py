bc9bd2703001a4623f92efc822d2e150a70fd880
nicolasluarte <l.luarte@uc.cl>
2025-03-16 02:32:25 -0300

    update

--- scripts/picam.py
+++ scripts/picam.py
@@ -32,21 +32,38 @@ print("Opening arduino connection...")
 
 # set this to match lickometer spec usual 115200 as baudrate
 ser = serial.Serial(arduino_ports[0])
-ser.baudrate = 9600
+ser.baudrate = 115200
 print("Connection ready!")
 
 # this loop searches for the arduino signal
 try:
     while True:
         data_raw = ser.readline()
-        data_decoded = data_raw.decode('latin-1')
-        if "START" in data_decoded:
-            print("Start recording...")
-            timestamp = str(time.time())
-            picam2.start_recording(encoder, "out_"+timestamp+".mp4")
-        elif "STOP" in data_decoded:
-            print("Stop recording...")
-            picam2.stop_recording()
+        #data_decoded = data_raw.decode('latin-1')
+        # for a json string this follow this form
+        # if(dict_name["key1"]["following_status"]=="followed")
+        # "following status" is within "key1"
+        # to adapt the code I just need "new_event", "new_poke"
+        # that way is easier to get a simple reading
+        # so if this is to start recording after nosepoke
+        # I read nosePoke_state
+        # if nosePoke_state == TRUE
+        # stop any previous recordings
+        # start recording file is going to be called animal_date_T_trialnumber
+        #if "START" in data_decoded:
+        #    print("Start recording...")
+        #    timestamp = str(time.time())
+        #    picam2.start_recording(encoder, "out_"+timestamp+".mp4")
+        # then here start a while loop
+        # while bussy_sensor signal is FALSE keep doing pass
+        # while (bussy_sensor_signal == 0):
+        #   pass
+        # after this stop recording, because this mean that animal triggered an event
+        # replay this previously saved file in desktop
+        # immediately start another recording with animal_date_L_(trialnumber - 1)
+        #elif "STOP" in data_decoded:
+        #    print("Stop recording...")
+        #    picam2.stop_recording()
 finally:
     print("Done")
         
